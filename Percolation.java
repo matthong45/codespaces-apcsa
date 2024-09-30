6133b09862050be0d71760eb7743b5c5fee45d7e
matthong45 <55729582+matthong45@users.noreply.github.com>
2024-09-30 14:57:49 -0700

    Removing the small change.

--- Percolation.java
+++ Percolation.java
@@ -10,7 +10,6 @@ public class Percolation {
     private final int tailIndex;
     private final boolean[][] openStatus;
     private int numOpen;
-    private int testingGitHub;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
